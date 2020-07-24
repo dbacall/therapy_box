ae1484ffc91c243e08c4a5dfbbe14f2100128558
dbacall <dbacall@hotmail.co.uk>
2020-07-24 10:50:27 +0100

    feat: user can search for team and see beaten teams

--- client/src/components/Sport.js
+++ client/src/components/Sport.js
@@ -7,6 +7,8 @@ class Sport extends Component {
     super(props);
     this.state = {
       data: null,
+      team: '',
+      beatenTeams: null,
     };
   }
 
@@ -16,10 +18,55 @@ class Sport extends Component {
     });
   }
 
+  onChange = (e) => {
+    this.setState({ team: e.target.value });
+  };
+
+  onSubmit = (e) => {
+    const { team } = this.state;
+    e.preventDefault();
+    var beatenTeams = this.state.data;
+    this.setState({
+      beatenTeams: beatenTeams
+        .filter((game) => {
+          return (
+            (game.HomeTeam === team && game.FTHG > game.FTAG) ||
+            (game.AwayTeam === team && game.FTAG > game.FTHG)
+          );
+        })
+        .map((game) => {
+          if (game.HomeTeam === team) {
+            return game.AwayTeam;
+          } else {
+            return game.HomeTeam;
+          }
+        }),
+    });
+  };
+
   render() {
+    const { data, beatenTeams } = this.state;
     return (
       <div>
         <h1>Champions League Challenge</h1>
+        {data ? (
+          <form onSubmit={this.onSubmit}>
+            <input
+              type="text"
+              placeholder="Input your team here..."
+              value={this.state.team}
+              onChange={this.onChange}
+            />
+            <input type="submit" value="Submit" />
+          </form>
+        ) : null}
+        {beatenTeams ? (
+          <ul>
+            {beatenTeams.map((team) => {
+              return <li>{team}</li>;
+            })}
+          </ul>
+        ) : null}
       </div>
     );
   }
