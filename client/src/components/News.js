18baf89f63fb574af78bec279aea80153b081d69
dbacall <dbacall@hotmail.co.uk>
2020-07-25 15:14:13 +0100

    style: news page

--- client/src/components/News.js
+++ client/src/components/News.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import '../stylesheets/News.css';
 
 class News extends Component {
   constructor(props) {
@@ -8,11 +9,13 @@ class News extends Component {
   render() {
     const { news } = this.props.location.state;
     return (
-      <div>
-        <h1>News</h1>
-        <img src={news[0].thumbnail} alt="News" />
-        <h3>{news[0].title}</h3>
-        <p>{news[0].content}</p>
+      <div className="news-container">
+        <h1 className="news-title">News</h1>
+        <div className="news-img-container">
+          <img src={news[0].thumbnail} alt="News" className="news-img" />
+        </div>
+        <h3 className="news-headline">{news[0].title}</h3>
+        <p className="news-content">{news[0].content}</p>
       </div>
     );
   }
