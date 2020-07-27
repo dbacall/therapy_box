77b2322f9265685c68ab1c429df123757557d327
dbacall <dbacall@hotmail.co.uk>
2020-07-27 15:05:52 +0100

    update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import Dashboard from './components/Dashboard';
 import Register from './components/Register';
