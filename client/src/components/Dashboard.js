65d7ca47a991c4f1b7fc7cb83fc92c2f62786d79
dbacall <dbacall@hotmail.co.uk>
2020-07-27 16:15:52 +0100

    update dashboard

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -129,7 +129,7 @@ class Dashboard extends Component {
       const lon = position.coords.longitude;
       axios
         .get(
-          `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=d0a10211ea3d36b0a6423a104782130e`
+          `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=d0a10211ea3d36b0a6423a104782130e`
         )
         .then((res) => {
           this.setState({ weather: res.data });
