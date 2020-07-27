241f00f2437ea09c71f2161e2f673a9caad87825
dbacall <dbacall@hotmail.co.uk>
2020-07-27 14:47:57 +0100

    update app.js

--- app.js
+++ app.js
@@ -31,4 +31,14 @@ app.use('/tasks', tasksRouter);
 app.use('/photos', photosRouter);
 app.use('/team', teamRouter);
 
+// Serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static(path.join(__dirname, 'client', 'build')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 module.exports = app;
